a979123a2521019306242e2cbdb2c104d4ee9914
StephenTomlin <Stephen.Tomlinson@live.com>
2017-02-13 14:11:04 -0500

    added a simple hello world and declared some variables

---
+++ VariablesTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Tutorial
+{
+    public static void Main()
+    {
+        // write your code here
+        string productName = "TV";
+        int productYear = 2012;
+        double productPrice = 279.99;
+        // test code
+    }
+}
